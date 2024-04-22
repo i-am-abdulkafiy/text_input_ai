fc61cf9ead90787c16be2f6113bb8ecde9a24a2e
Abdulkafi <124082178+i-am-abdulkafiy@users.noreply.github.com>
2024-04-22 15:10:24 +0100

    Add files via upload

---
+++ text_ai.py
@@ -0,0 +1,7 @@
+'''Text Input Ai'''
+
+#Creating variable to text_input (name)
+name = input('What\'s your name? ')
+
+#Printing welcome message alongside with your name
+print('You\'re welcome %s'%name)
